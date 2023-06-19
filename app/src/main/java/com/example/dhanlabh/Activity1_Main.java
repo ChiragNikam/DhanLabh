fb89e645900b4a3a593989423f535649ff2946a3
ChiragNikam <chiragnikam02@gmail.com>
2023-06-19 22:55:23 +0530

    refactor- Changed filenames, java file and some basic things regarding code readablity

--- app/src/main/java/com/example/dhanlabh/Activity1_Main.java
+++ app/src/main/java/com/example/dhanlabh/Activity1_Main.java
@@ -4,35 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static final String INTENT_STRING = "com.example.dhanlabh.sending_intent";
+    public ArrayList<String> exp_category = new ArrayList<String>();    // this will store category of expense.
+    public ArrayList<Integer> exp_amount = new ArrayList<Integer>();    // this will store amount of expense
 //    public String[] contents_of_list_view = {"Income and Expenses", "Transaction History", "Budget Analysis", "Goal Tracker"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "at on Create", Toast.LENGTH_SHORT).show();
+        exp_category.add("Daru");   // adding entry of category to the array list
+        exp_amount.add(20);         // adding entry of amount to the array list
     }
     public void gotoIncomeAndExpense(View view){ // directing to 2nd activity for Income and Expence
-        Intent intent = new Intent(this, MainActivity2.class);
+        Intent intent = new Intent(this, ExpenseAndHistory.class);
         intent.putExtra(INTENT_STRING, "Expenses and History");
         startActivity(intent);
     }
     public void gotoYourSpendings(View view){   // directing to 2nd activity for Spendings
-        Intent intent = new Intent(this, MainActivity2.class);
+        Intent intent = new Intent(this, ExpenseAndHistory.class);
         intent.putExtra(INTENT_STRING, "Your Spendings");
         startActivity(intent);
     }
     public void gotoYourBudgets(View view){ // directing to 2nd activity for Budgets
-        Intent intent = new Intent(this, MainActivity2.class);
+        Intent intent = new Intent(this, ExpenseAndHistory.class);
         intent.putExtra(INTENT_STRING, "Your Budget");
         startActivity(intent);
     }
     public void gotoTrackAndSetGoals(View view){    // directing to 2nd activity for Future Financial Goals
-        Intent intent = new Intent(this, MainActivity2.class);
+        Intent intent = new Intent(this, ExpenseAndHistory.class);
         intent.putExtra(INTENT_STRING, "Track and Set Goals");
         startActivity(intent);
     }
